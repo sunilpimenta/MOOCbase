0d9598b1f38edfef591214e3509b91b429778368
David Shi <shi501331@berkeley.edu>
2020-12-29 11:43:35 -0600

    retry uploading files

--- fa20-proj5-DavidShi17/parsergen.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-rm -rf src/main/java/edu/berkeley/cs186/database/cli/parser
-jjtree MoocParser.jjt
-javacc src/main/java/edu/berkeley/cs186/database/cli/parser/MoocParser.jj
-rm src/main/java/edu/berkeley/cs186/database/cli/parser/MoocParser.jj
